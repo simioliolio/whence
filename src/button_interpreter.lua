47fc7c965d9fd0367e60beae2cfa7a4a4e1fc33d
Simon Haycock <shaycock@spotify.com>
2025-05-02 15:22:47 +0100

    Add transport when page button held

--- src/button_interpreter.lua
+++ src/button_interpreter.lua
@@ -3,21 +3,34 @@ local ButtonInterpreter = {}
 function ButtonInterpreter.new()
   local self = {
     sequencer_page = 0,
+    play_stop_toggled = nil,  -- Start as nil
+    held_page_button = nil,  -- Track which page button is being held
     listeners = {
-      on_page_change = nil
+      on_change = nil -- set this to receive updates when the state changes
     }
   }
 
   function self:handle_press(button_data)
     local x, y, state = table.unpack(button_data)
     
-    -- Process button presses (state = 1)
-    if state == 1 then
-      -- Handle page selection using row 7 (y = 7)
-      if y == 7 then
-        self.sequencer_page = x + 1  -- Adding 1 since Lua arrays are 1-based
-        if self.listeners.on_page_change then
-          self.listeners.on_page_change(self)
+    if state == 1 then  -- Button press
+      if y == 7 then  -- Page selection row
+        self.held_page_button = x
+        self.sequencer_page = x + 1
+        if self.listeners.on_change then
+          self.listeners.on_change(self)
+        end
+      elseif y == 6 and self.held_page_button ~= nil then  -- Transport row with page button held
+        self.play_stop_toggled = not self.play_stop_toggled
+        if self.listeners.on_change then
+          self.listeners.on_change(self)
+        end
+      end
+    else  -- Button release
+      if y == 7 then  -- Page button released
+        -- Only clear held_page_button if this was the currently held button
+        if self.held_page_button == x then
+          self.held_page_button = nil
         end
       end
     end
--- test/src/button_interpreter_spec.lua
+++ test/src/button_interpreter_spec.lua
@@ -8,7 +8,7 @@ describe("ButtonInterpreter", function()
     local listener = function(model)
       captured_model = model
     end
-    button_interpreter.listeners.on_page_change = listener
+    button_interpreter.listeners.on_change = listener
 
     -- Test pages 1-4 using buttons 0-3
     for x = 0, 3 do
@@ -16,5 +16,54 @@ describe("ButtonInterpreter", function()
       assert(captured_model.sequencer_page == x + 1, 
              string.format("Expected page %d when pressing button %d", x + 1, x))
     end
+  end)
+
+  it("should interpret row 6 as transport when page button held", function()
+    local button_interpreter = ButtonInterpreter.new()
+    local captured_model = nil
+    local listener = function(model)
+      captured_model = model
+    end
+    button_interpreter.listeners.on_change = listener
+
+    -- Initial state
+    assert(button_interpreter.play_stop_toggled == nil, "Initial state should be false")
+
+    -- Press page button
+    button_interpreter:handle_press({0,7,1})
+    assert(button_interpreter.held_page_button == 0, "Page button should be held")
+
+    -- Press transport button
+    button_interpreter:handle_press({0,6,1})
+    assert(button_interpreter.play_stop_toggled == true, "Transport should be toggled")
+  end)
+
+  it("should interpret row 6 as transport when page buttons held", function()
+    local button_interpreter = ButtonInterpreter.new()
+    local captured_model = nil
+    local listener = function(model)
+      captured_model = model
+    end
+    button_interpreter.listeners.on_change = listener
+
+    -- A slightly more complex test involving when the page button is released
+
+    -- Initial state
+    assert(button_interpreter.play_stop_toggled == nil, "Initial state should be false")
+
+    -- Press page button
+    button_interpreter:handle_press({0,7,1})
+    assert(button_interpreter.held_page_button == 0, "Page button 0 should be held")
+
+    button_interpreter:handle_press({1,7,1})
+    button_interpreter:handle_press({0,7,0})
+    assert(button_interpreter.held_page_button == 1, "Page button 1 should be held")
+
+    -- Press transport button
+    button_interpreter:handle_press({0,6,1})
+    assert(button_interpreter.play_stop_toggled == true, "Transport should be toggled")
+    button_interpreter:handle_press({1,7,0})
+    assert(button_interpreter.held_page_button == nil, "Page button 1 should be held")
+    button_interpreter:handle_press({0,6,1})
   end)
 end)
