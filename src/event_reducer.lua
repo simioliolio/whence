003fd5440e0c65719b1d9c542182b906a9838b74
Simon Haycock <shaycock@spotify.com>
2025-05-04 21:07:55 +0100

    Fix old button event name in reducer

--- src/event_reducer.lua
+++ src/event_reducer.lua
@@ -5,16 +5,16 @@ function EventReducer.new()
 
   -- Returns a _modified_ state based on the button model
   function self:reduce(button_event, state)
-    state.selected_page = event_model.sequencer_page
-    if event_model.play_stop_toggled_event then
+    state.selected_page = button_event.sequencer_page
+    if button_event.play_stop_toggled_event then
       state.play_requested = not state.play_requested
     end
-    if event_model.held_page_button then
+    if button_event.held_page_button then
       state.showing_transport_row = true
     else
       state.showing_transport_row = false
     end
-    state.latest_button_event = event_model
+    state.latest_button_event = button_event
     return state
   end
 
