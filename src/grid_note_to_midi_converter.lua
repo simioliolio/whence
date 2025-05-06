1189244c5387725b4115a9e3195f9dcbd71e5972
Simon Haycock <shaycock@spotify.com>
2025-05-06 09:49:13 +0100

    Add tests and fix bugs in grid note midi converter

--- src/grid_note_to_midi_converter.lua
+++ src/grid_note_to_midi_converter.lua
@@ -2,8 +2,9 @@ local GridNoteToMidiConverter = {}
 
 GridNoteToMidiConverter.ROWS = 5
 GridNoteToMidiConverter.COLS = 16
-GridNoteToMidiConverter.BOTTOM_ROW = 4
-GridNoteToMidiConverter.TOP_ROW = 0
+
+GridNoteToMidiConverter.BOTTOM_ROW = 5
+GridNoteToMidiConverter.TOP_ROW = 1
 
 -- The number of chromatic steps jumped between grid notes in one column
 GridNoteToMidiConverter.ROW_NOTE_DELTA = 5
@@ -14,12 +15,13 @@ function GridNoteToMidiConverter.new()
   }
 
   function self:convert(grid_note)
-    assert(grid_note.x >= 0 and grid_note.x < GridNoteToMidiConverter.COLS, "Invalid grid note x")
-    assert(grid_note.y >= 0 and grid_note.y < GridNoteToMidiConverter.ROWS, "Invalid grid note y")
+    assert(grid_note.x >= 1 and grid_note.x <= GridNoteToMidiConverter.COLS, "Invalid grid note x")
+    assert(grid_note.y >= 1 and grid_note.y <= GridNoteToMidiConverter.ROWS, "Invalid grid note y")
     -- Invert the y axis so that the bottom row is 0 and the top row is 4
-    local inverted_y = GridNoteToMidiConverter.ROWS - grid_note.y
-    local y_midi = (inverted_y * GridNoteToMidiConverter.ROW_NOTE_DELTA)
-    return self.lowest_midi_note + y_midi + grid_note.x
+    local inverted_y_scalar = GridNoteToMidiConverter.ROWS - grid_note.y
+    local y_midi_delta = (inverted_y_scalar * GridNoteToMidiConverter.ROW_NOTE_DELTA)
+    local x_midi_delta = grid_note.x - 1
+    return self.lowest_midi_note + y_midi_delta + x_midi_delta
   end
 
   return self
--- test/src/grid_note_to_midi_converter_spec.lua
+++ test/src/grid_note_to_midi_converter_spec.lua
@@ -2,10 +2,32 @@ require "busted"
 local GridNoteToMidiConverter = require "src.grid_note_to_midi_converter"
 
 describe("GridNoteToMidiConverter", function()
-  it("should convert grid note to midi note", function()
+  it("should convert lowestgrid note to midi note", function()
     local converter = GridNoteToMidiConverter.new()
-    local grid_note = {x = 0, y = 0}
+    local grid_note = {x = 1, y = 5}
     local midi_note = converter:convert(grid_note)
-    assert.are.equal(midi_note, 46)
+    assert.are.equal(21, midi_note)
+  end)
+
+  it("should offset the midi note by the lowest midi note", function()
+    local converter = GridNoteToMidiConverter.new()
+    converter.lowest_midi_note = 40
+    local grid_note = {x = 1, y = 5}
+    local midi_note = converter:convert(grid_note)
+    assert.are.equal(40, midi_note)
+  end)
+
+  it("should convert grid note to midi note respecting x", function()
+    local converter = GridNoteToMidiConverter.new()
+    local grid_note = {x = 2, y = 5}
+    local midi_note = converter:convert(grid_note)
+    assert.are.equal(22, midi_note)
+  end)
+
+  it("should convert grid note to midi note respecting y", function()
+    local converter = GridNoteToMidiConverter.new()
+    local grid_note = {x = 1, y = 4}
+    local midi_note = converter:convert(grid_note)
+    assert.are.equal(26, midi_note)
   end)
 end)
