8ff72b2583d7a11cbffac217702aaefbe846fe7d
Simon Haycock <shaycock@spotify.com>
2025-05-06 15:16:56 +0100

    Beginnings of a coordinator to tie things together

---
+++ src/coordinator.lua
@@ -0,0 +1,23 @@
+
+local Coordinator = {}
+
+function Coordinator.new()
+  local self = {
+    -- set externally
+    led_set = function (_,_,_) assert(false, "not set") end,
+    led_refresh = function() assert(false, "not set") end,
+
+    grid_button_handler = function (x,y,z) self:handle_button(x,y,z) end,
+  }
+
+  function Coordinator:start()
+    -- TODO: Setup
+  end
+
+  function Coordinator:handle_button(x,y,z)
+    print("handling button " .. x .. y .. z)
+  end
+end
+
+return Coordinator
+
--- whence.lua
+++ whence.lua
@@ -5,5 +5,13 @@
 -- under src/ (for unit testing)
 --
 
-g = grid.connect()
-g.key = function(x,y,z) print(x,y,z) end
+require "src.coordinator"
+
+function init()
+  local coordinator = Coordinator.new()
+  g = grid.connect()
+  g.key = coordinator.grid_button_handler
+  coordinator.led_set = g.led
+  coordinator.led_refresh = g.refresh
+  coordinator.start()
+end
