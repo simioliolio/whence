6db476bd2fd50240e5524c374e8ab830f9a1f65e
Simon Haycock <shaycock@spotify.com>
2025-05-07 13:50:50 +0100

    Hide state and provide getter

--- lib/state_modifier.lua
+++ lib/state_modifier.lua
@@ -1,16 +1,22 @@
 local StateModifier = {}
 
 function StateModifier.new(initial_state)
+  -- Keep state in closure scope
+  local state = initial_state
+
   local self = {
-    state = initial_state,
     listeners = {
       on_state_change = nil
     }
   }
 
+  function self:get_state()
+    return state
+  end
+
   function self:reduce(event, reducer)
-    local new_state = reducer:reduce(event, self.state)
-    self.state = new_state
+    local new_state = reducer:reduce(event, state)
+    state = new_state
     if self.listeners.on_state_change then
       self.listeners.on_state_change(new_state)
     end
