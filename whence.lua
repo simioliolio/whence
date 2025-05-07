f01279dec5b8b474963925b20fde3eac9d4f487c
Simon Haycock <shaycock@spotify.com>
2025-05-07 13:26:40 +0100

    Move Coordinator to whence.lua

--- whence.lua
+++ whence.lua
@@ -5,7 +5,28 @@
 -- under lib/ (for unit testing)
 --
 
-local Coordinator = require "whence.lib.coordinator"
+local Coordinator = {}
+
+function Coordinator.new()
+  local self = {
+    -- set externally
+    led_set = function (_,_,_) assert(false, "not set") end,
+    led_refresh = function() assert(false, "not set") end,
+
+    grid_button_handler = function (x,y,z) self:handle_button(x,y,z) end,
+  }
+
+  function self:start()
+    print("start called")
+  end
+
+  function self:handle_button(x,y,z)
+    print("handling button " .. x .. y .. z)
+  end
+
+  return self
+end
+
 
 function init()
   local coordinator = Coordinator.new()
